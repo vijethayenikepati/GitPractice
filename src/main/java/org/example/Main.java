4db09fca9f1ff29c0c1fb1d92167efda291cd108
vijethayenikepati <vijetha3y@gmail.com>
2024-10-31 21:41:36 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ Main {
         System.out.println("Hello world!");
         System.out.println("Feature1 branch created in local and pushed to master!");
         System.out.println("Feature 1 first change");
-        System.out.println("Edit in Master branch");
+        System.out.println("Edit in Master branch, First");
         System.out.println("Edit2 in Master branch");
         System.out.println("Edit3 in Master branch");
         System.out.println("Feature 1 second change");
